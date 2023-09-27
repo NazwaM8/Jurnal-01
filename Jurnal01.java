42f5d8c9b9ddbb967807125d6e562486f584a9d6
Nazwa Mursyidan Baldan <Nizuuichi@gmail.com>
2023-09-27 09:31:22 +0700

    Jurnal 01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama = "Nazwa Mursyidan Baldan";
+        String nim = "607062300077";
+        String kelas = "D3IF-47-04";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+}
